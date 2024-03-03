655f0ffa1eee2c4825966b02f7f88728c6830f65
InaccurateTank <inaccuratetank@outlook.com>
2024-03-03 15:56:34 -0800

    legacy render issue?

--- hosts/sabot/default.nix
+++ hosts/sabot/default.nix
@@ -36,13 +36,6 @@
     '';
   };
 
-  # programs = {
-  #   hyprland = {
-  #     enable = true;
-  #     xwayland.enable = true;
-  #   };
-  # };
-
   services.openssh.enable = true;
 
   # xdg.portal = {
@@ -59,8 +52,6 @@
   nix = {
     settings = {
       auto-optimise-store = true;
-      substituters = ["https://hyprland.cachix.org"];
-      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
       experimental-features = ["nix-command" "flakes"];
     };
     # gc = {
