dba9e8c0ff7c2638ada8124f1d6eaa6f10469404
InaccurateTank <inaccuratetank@outlook.com>
2024-03-05 18:15:17 -0800

    webgpu under x fails + empty default

--- users/inacct/wezterm/wezterm.lua
+++ users/inacct/wezterm/wezterm.lua
@@ -37,13 +37,13 @@ config.font = wez.font('Iosevka Nerd Font')
 config.font_size = 11.0
 config.color_scheme_dirs = {'$HOME/.config/wezterm/colors'}
 config.color_scheme = "HorizonDark"
-config.default_prog = default_prog
+-- config.default_prog = default_prog
 config.initial_rows = 35
 config.initial_cols = 125
 config.use_fancy_tab_bar = false
 config.tab_max_width = 60
 config.enable_scroll_bar = true
-config.launch_menu = launch_menu
+-- config.launch_menu = launch_menu
 config.window_background_opacity = 0.95
 config.status_update_interval = 2000
 config.mouse_bindings = require 'utils.mousebinds'
@@ -52,13 +52,13 @@ config.mouse_bindings = require 'utils.mousebinds'
 config.default_cursor_style = "BlinkingUnderline"
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 -- Rendering issues on default webgpu choice, needs Gl
-config.front_end = 'WebGpu'
-for _, gpu in ipairs(wez.gui.enumerate_gpus()) do
-	if gpu.backend == "Gl" then
-		config.webgpu_preferred_adapter = gpu
-		break
-	end
-end
+-- config.front_end = 'WebGpu'
+-- for _, gpu in ipairs(wez.gui.enumerate_gpus()) do
+-- 	if gpu.backend == "Gl" then
+-- 		config.webgpu_preferred_adapter = gpu
+-- 		break
+-- 	end
+-- end
 config.enable_wayland = false
 
 return config
