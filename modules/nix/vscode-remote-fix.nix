d4ca1acecdadb2b240b34388341760739320710f
InaccurateTank <inaccuratetank@outlook.com>
2024-03-14 00:11:38 -0700

    permissions issues fixed

--- modules/nix/vscode-remote-fix.nix
+++ modules/nix/vscode-remote-fix.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: {
   imports = [
@@ -11,12 +12,12 @@
     wget
   ];
 
-  system.activationScripts.userList = with lib; let
+  system.activationScripts.vscodeFixUsers = with lib; let
     fixFile = pkgs.writeText "vscodeRemoteFix" ''
       PATH=$PATH:/run/current-system/sw/bin/
     '';
     realUsers = filterAttrs (_: v: v.isNormalUser && v.home != "/var/empty") config.users.users;
-    cmdList = mapAttrsToList (_: v: "mkdir -p ${v.home}/.vscode-server && ln -fs ${fixFile} ${v.home}/.vscode-server/server-env-setup") realUsers;
+    cmdList = mapAttrsToList (_: v: "install -d -m 755 -o ${v.name} -g users ${v.home}/.vscode-server && ln -fs ${fixFile} ${v.home}/.vscode-server/server-env-setup") realUsers;
   in
     concatStringsSep "\n" cmdList;
 }
