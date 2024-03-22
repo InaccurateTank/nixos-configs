d9d58c56c5c967fd5d8dedda4d584132f1db15c8
InaccurateTank <inaccuratetank@outlook.com>
2024-03-22 05:10:40 -0700

    Security pass via stealing dotfiles

--- modules/nix/security.nix
+++ modules/nix/security.nix
@@ -1,10 +1,22 @@
-{...}: {
+{
+  lib,
+  config,
+  ...
+}: {
+  # Enable Firewall
   networking.firewall.enable = true;
+
+  # Only sudoers can use nix
+  # Would be "root" but that breaks home-manager
   nix.settings.allowed-users = ["@wheel"];
+
+  # Basic sudo stuff
   security.sudo = {
     execWheelOnly = true;
     wheelNeedsPassword = true;
   };
+
+  # SSH Security
   services.openssh = {
     allowSFTP = false;
     settings = {
@@ -18,5 +30,24 @@
       AllowStreamLocalForwarding no
       AuthenticationMethods publickey
     '';
+  };
+
+  # *Completely* disable root login
+  users.users.root.hashedPassword = "!";
+
+  # AppArmor
+  security.apparmor = {
+    enable = true;
+    packages = with pkgs; [
+      apparmor-utils
+      apparmor-profiles
+    ];
+  };
+
+  # Enable TPM support
+  security.tpm2 = {
+    enable = true;
+    pkcs11.enable = true;
+    tctiEnvironment.enable = true;
   };
 }
