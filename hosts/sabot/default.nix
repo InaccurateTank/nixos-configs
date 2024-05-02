1608d7824b916dc7538553c7d0c68b065a02cbd4
InaccurateTank <inaccuratetank@outlook.com>
2024-05-01 19:35:35 -0700

    need to enable flatpak

--- hosts/sabot/default.nix
+++ hosts/sabot/default.nix
@@ -119,6 +119,7 @@
       settings.default_session.command = "${pkgs.cage}/bin/cage -ds -m last ${inputs.ags.packages.${pkgs.system}.ags}/bin/ags -- -c ${./greeter.js}";
     };
     xserver.videoDrivers = ["amdgpu"];
+    flatpak.enable = true;
   };
 
   hardware.opengl = {
