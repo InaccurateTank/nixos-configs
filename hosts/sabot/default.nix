0c278d737540a702477b95a9bd6746d859d1c37b
InaccurateTank <inaccuratetank@outlook.com>
2024-05-25 23:34:03 -0700

    full silent boot snippet

--- hosts/sabot/default.nix
+++ hosts/sabot/default.nix
@@ -14,12 +14,25 @@
   };
 
   boot = {
-    initrd.verbose = false;
     plymouth = {
       enable = true;
       theme = "nixos-bgrt";
       themePackages = [pkgs.nixos-bgrt-plymouth];
     };
+
+    # Silent Boot
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+
     loader = {
       efi.canTouchEfiVariables = true;
       grub = {
