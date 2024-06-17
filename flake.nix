c23d98f49385b3376a0ca081bda5d5ad5205b5d0
InaccurateTank <inaccuratetank@outlook.com>
2024-06-16 23:20:12 -0700

    smth with hyprland idk

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
       url = "github:hyprwm/contrib";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     # hyprland.inputs.nixpkgs.follows = "nixpkgs";
     hyprland-plugins = {
       url = "github:hyprwm/hyprland-plugins";
