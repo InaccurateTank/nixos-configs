f1fc6aa5170a199a0a8ac47f194f32f5963808ec
InaccurateTank <inaccuratetank@outlook.com>
2024-09-18 06:43:19 -0700

    somthing seems fixed, commenting instead of removing

--- modules/nix/vscode-remote-fix.nix
+++ modules/nix/vscode-remote-fix.nix
@@ -13,13 +13,13 @@ in {
 
     programs.nix-ld.enable = lib.mkForce true;
 
-    system.activationScripts.vscodeFixUsers = with lib; let
-      fixFile = pkgs.writeText "vscodeRemoteFix" ''
-        PATH=$PATH:/run/current-system/sw/bin/
-      '';
-      realUsers = filterAttrs (_: v: v.isNormalUser && v.home != "/var/empty") config.users.users;
-      cmdList = mapAttrsToList (_: v: "install -d -m 755 -o ${v.name} -g users ${v.home}/.vscode-server && ln -fs ${fixFile} ${v.home}/.vscode-server/server-env-setup") realUsers;
-    in
-      concatLines cmdList;
+    # system.activationScripts.vscodeFixUsers = with lib; let
+    #   fixFile = pkgs.writeText "vscodeRemoteFix" ''
+    #     PATH=$PATH:/run/current-system/sw/bin/
+    #   '';
+    #   realUsers = filterAttrs (_: v: v.isNormalUser && v.home != "/var/empty") config.users.users;
+    #   cmdList = mapAttrsToList (_: v: "install -d -m 755 -o ${v.name} -g users ${v.home}/.vscode-server && ln -fs ${fixFile} ${v.home}/.vscode-server/server-env-setup") realUsers;
+    # in
+    #   concatLines cmdList;
   };
 }
