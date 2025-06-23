05f4529a7ac9a45355fda4dc2cfc5825678f4aa3
InaccurateTank <inaccuratetank@outlook.com>
2025-06-22 22:58:55 -0700

    cs bouncer is in nixpkgs now and i don't use the font

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,9 +1,4 @@
 pkgs: {
-  iosevka-tonk = pkgs.callPackage ./iosevka-tonk {};
-  iosevka-tonk-term = pkgs.callPackage ./iosevka-tonk {set = "term";};
-
-  cs-firewall-bouncer = pkgs.callPackage ./cs-firewall-bouncer.nix {};
-
   custom-caddy = pkgs.caddy.withPlugins {
     plugins = ["github.com/caddy-dns/porkbun@v0.2.1"];
     hash = "sha256-ayd1WnjBjQOIXtiCkR/aWML2Nc4eRyuTugsjHXOU5uQ=";
