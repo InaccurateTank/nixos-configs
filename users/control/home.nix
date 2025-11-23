846fa503875cd9eed91ce26aee9274f4e9ab221c
InaccurateTank <inaccuratetank@outlook.com>
2025-11-22 19:31:12 -0800

    Enabling home-manager zsh for control

--- users/control/home.nix
+++ users/control/home.nix
@@ -1,6 +1,17 @@
 {...}: {
+  home = {
+    username = "control";
+    homeDirectory = "/home/control";
+    stateVersion = "25.11";
+  };
+
   flakeMods.zsh-inits = {
     arrowFix = true;
     oscSequences = true;
+  };
+
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;
   };
 }
