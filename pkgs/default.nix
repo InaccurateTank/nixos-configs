9f284cba340c4145b31719ccf30da10e77197cdd
InaccurateTank <inaccuratetank@outlook.com>
2025-11-23 00:44:12 -0800

    caddy hash update

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,6 @@
 pkgs: {
   custom-caddy = pkgs.caddy.withPlugins {
     plugins = ["github.com/caddy-dns/porkbun@v0.3.1"];
-    hash = "sha256-PUHu+KPywdJMuPLHPtQhUaw3Cv1pED5XQ1MOzlT/6h4=";
+    hash = "sha256-NlZY/EEY9TbqrMAkSHK2aGm5AjFTvpvBXV1GW1PnXCc=";
   };
 }
