6f429b2dd4fc35df4ae5b81f4af7a9a6503a1279
InaccurateTank <inaccuratetank@outlook.com>
2025-12-15 17:07:52 -0800

    Crowdsec etc creation

--- hosts/canister/crowdsec.nix
+++ hosts/canister/crowdsec.nix
@@ -10,6 +10,8 @@
   systemd.tmpfiles.rules = [
     # See https://github.com/NixOS/nixpkgs/issues/445342
     "d /var/lib/crowdsec 0750 crowdsec crowdsec - -"
+    # Stuff is created here, should make it manually.
+    "d /etc/crowdsec 0750 crowdsec crowdsec - -"
     # In contrast to the `lapi.credentialsFile`, the `capi.credentialsFile` must already exist beforehand
     "f /var/lib/crowdsec/online_api_credentials.yaml 0750 crowdsec crowdsec - -"
   ];
