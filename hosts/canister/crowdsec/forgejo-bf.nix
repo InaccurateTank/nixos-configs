e2eaf6cdfa45f66136b0399b339b7ce66ad78e76
InaccurateTank <inaccuratetank@outlook.com>
2025-12-16 11:45:09 -0800

    Add forgejo-slow-bf

--- hosts/canister/crowdsec/forgejo-bf.nix
+++ hosts/canister/crowdsec/forgejo-bf.nix
@@ -1,7 +1,7 @@
 [
   {
     name = "inaccuratetank/forgejo-bf";
-    description = "Detect gitea bruteforce";
+    description = "Detect Forgejo bruteforce";
     filter = "evt.Meta.log_type == 'forgejo_failed_auth'";
     type = "leaky";
     groupby = "evt.Meta.source_ip";
@@ -15,6 +15,25 @@
       spoofable = 0;
       confidence = 3;
       label = "Forgejo Bruteforce";
+      remediation = true;
+    };
+  }
+  {
+    name = "inaccuratetank/forgejo-slow-bf";
+    description = "Detect slow Forgejo bruteforce";
+    filter = "evt.Meta.log_type == 'forgejo_failed_auth'";
+    type = "leaky";
+    groupby = "evt.Meta.source_ip";
+    leakspeed = "60s";
+    capacity = 10;
+    blackhole = "1m";
+    labels = {
+      service = "forgejo";
+      behavior = "vcs:bruteforce";
+      classification = ["attack.T1110"];
+      spoofable = 0;
+      confidence = 3;
+      label = "Forgejo Slow Bruteforce";
       remediation = true;
     };
   }
